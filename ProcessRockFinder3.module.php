1193f33d4b32ab9772b7ee9300996b05ba2ad68f
BernhardBaumrock <office@baumrock.com>
2020-05-22 17:18:29 +0200

    auto-open console

--- ProcessRockFinder3.module.php
+++ ProcessRockFinder3.module.php
@@ -33,7 +33,7 @@ class ProcessRockFinder3 extends Process {
   }
 
   /**
-   * 
+   * Use Tracy Console to debug and test finders
    */
   public function execute() {
     $this->headline('RockFinder3 Tester');
@@ -41,16 +41,17 @@ class ProcessRockFinder3 extends Process {
     $nl = "\n";
     $this->config->scripts->add("https://unpkg.com/tabulator-tables@4.6.3/dist/js/tabulator.min.js");
     $this->config->styles->add("https://unpkg.com/tabulator-tables@4.6.3/dist/css/tabulator.min.css");
-    return '<strong>Open the tracy console and try this dump:</strong>
-      <pre><code>$f = new RockFinder2();'.$nl
-      .'$f->find("template=admin");'.$nl
-      .'$f->addColumns(["title", "created", "status"]);'.$nl
-      .'$f->dump();</code></pre>
-      <style>#tracy-debug-panel-ConsolePanel {
+    return "<style>
+      #tracy-debug-panel-ConsolePanel {
         left: 0 !important;
         top: 0 !important;
         width: 100vw !important;
         height: 100vh !important;
-      }</style>';
+      }
+      </style>
+      <script>
+      // open the console automatically
+      localStorage.setItem('tracy-debug-panel-ConsolePanel', '{}');
+      </script>";
   }
 }
